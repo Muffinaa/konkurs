f0429e27c3c8dec0643df402ea7239523714cd7d
Beziiii <114175554+Beziiii@users.noreply.github.com>
2022-10-13 19:43:08 +0200

    Elo

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import { deepOrange, deepPurple } from '@mui/material/colors';
+
+
+
+
+  return (
+    <Box>
+      <Stack direction="row" spacing={2}>
+      <Avatar>US</Avatar>
+      </Stack>
+    
+
+
+
+
+
+
+    </Box>
+  );
