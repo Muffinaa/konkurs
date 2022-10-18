701aa1b62755df9f2748c8e1fd0aaccea8ef9a53
Beziiii <114175554+Beziiii@users.noreply.github.com>
2022-10-18 22:47:56 +0200

    Updejt

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,16 +3,21 @@ import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { Typography } from "@mui/material";
-
 const Profile = () => {
   return (
+  <Box
+    sx={{bgcolor:"#0d47a1"}}
+  
+  >
     <Box 
-      sx = {{border:'2px dashed black', width:550, height:700 , position:'absolute' ,  left:'33%' , top:'15%',
-             padding:'55px'
+      sx = {{border:'2px dashed black', width:300, height:600 , position:'absolute' ,  left:'67%' , top:'15%',
+             padding:'55px', borderRadius: '0% 0% 0% 0%'
             }} > 
             
       <Box
-      sx = {{border:'2px dashed black', width:'100%', height:150, left:0,top:0, position:"absolute"}}
+      sx = {{borderBottom:'5px solid black', width:'100%', height:150, left:0,top:0, position:"absolute",
+      bgcolor: 'primary.main'
+      }}
       
       >
 
@@ -20,7 +25,7 @@ const Profile = () => {
       </Box>
       
         <Stack direction="row"
-          sx={{ position:'absolute', left:'13%', top:'12%', border:'2px dashed black', borderRadius:40, 
+          sx={{ position:'absolute', left:'36%', top:'12%', border:'4px solid black', borderRadius:40
             
           }}
         >
@@ -32,12 +37,34 @@ const Profile = () => {
         </Stack>
         <Typography
           variant="h5"
-          sx={{ position:"absolute", top:'27%', left:'9%' }}
+          sx={{ position:"absolute", top:'29%', left:'29%', opacity:'80%' }}
         >
             Name Surname
           </Typography>
+
+        <Typography
+
+          variant="h7"
+          sx={{position:"absolute", top:'36%', left:'43%', opacity:'70%'}}
+        >
+          E-mail
+
+          
+        </Typography>
+
+        <Typography
+
+          variant="h7"
+          sx={{position:"absolute", top:'41%', left:'43%', opacity:'70%'}}
+        >
+          Phone
+
+          
+        </Typography>
       
+    
     </Box>
+  </Box>
   );
 };
 
