6b1837732e9785c94d2bb2bf293be2b64fb7e480
Bartekkoks69 <115359956+Bartekkoks69@users.noreply.github.com>
2022-10-22 18:18:40 +0200

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,41 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Stack from "@mui/material/Stack";
+import react from "react";
+import { Card, Typography } from "@mui/material";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Button from '@mui/material/Button';
 
-const Profile = () => {
+
+export const Login = () => {
+
+
   return (
-    <Box>
-      <Stack direction="row" spacing={2}>
-        <Avatar>US</Avatar>
-      </Stack>
+    <Card sx={{ width: "350px", height: '450px', padding: "20px", backgroundColor: 'white', boxShadow: '3px 2px 9px lightblue',
+    margin: '4em', padding: '3em', }}>
+      <AccountCircleIcon color="primary" sx={{fontSize: '60px', marginLeft: '145px' }} />
+      <Typography 
+            sx={{ flexGrow: 3, fontSize: 30, textAlign: 'center', display: { xs: "none", sm: "block" } }}
+          >
+            Logowanie
+          </Typography>
+          <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch', marginLeft: '65px' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="First name" variant="outlined" />
+      <TextField id="outlined-basic1" label="Last name" variant="outlined" />
+      <TextField id="outlined-basic2" label="Email" variant="outlined" />
+      <TextField id="outlined-basic3" label="Password" variant="outlined" />
     </Box>
+    <Button variant="contained" size="large" sx={{ marginLeft: '125px' }}>
+          Login
+        </Button>
+    </Card>
   );
 };
 
-export default Profile;
+export default Login;
